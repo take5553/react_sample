70a5b638cd2f387f4fd04016f769418a7621fb7b
take555 <yamazakitakeshi02@gmail.com>
2018-11-12 22:38:27 +0900

    This is the first commit.

---
+++ app.js
@@ -0,0 +1,87 @@
+import React, { Component } from 'react';
+import ReactDOM from "react-dom";
+
+function List(props){
+	return (
+		<ul>
+			{props.todo.map( (todo, i) => {
+				return <li key={i}> <input type="button" value="×" onClick={() => props.deleteTodo(i)}/> {todo.title}</li>
+			})}
+		</ul>
+	)
+};
+
+
+
+class Input extends Component {
+	constructor(props){
+		super(props);
+		this.addTodo = this.addTodo.bind(this);
+	}
+	addTodo(){
+		this.props.addTodo(this.refs.newText.value);
+		this.refs.newText.value='';
+	}
+	render(){
+		return (
+			<div>
+				<input text="text" ref="newText"/>
+				<input type="button" value="追加" onClick={this.addTodo}/>
+			</div>
+		)
+	}
+};
+
+class App extends Component {
+
+	constructor(props){
+		super(props);
+		this.state = {
+			todo: [
+				{ title: 'JavaScript覚える' },
+				{ title: 'jQuery覚える' },
+				{ title: 'ES2015覚える' },
+				{ title: 'React覚える' }
+			]
+		};
+		this.addTodo = this.addTodo.bind(this);
+		this.deleteTodo = this.deleteTodo.bind(this);
+	}
+
+	// 新規追加
+	addTodo(value) {
+		// 追加
+		this.state.todo.push({
+			title: value
+		});
+		// 保存
+		this.setState({
+			todo : this.state.todo
+		});
+	}
+
+	// 削除機能
+	deleteTodo(i) {
+		// 削除
+		this.state.todo.splice(i,1);
+		// 保存
+		this.setState({
+			todo : this.state.todo
+		});
+	}
+
+	render() {
+		return (
+			<div>
+				<h1>TODOアプリ</h1>
+				<List todo={this.state.todo} deleteTodo={this.deleteTodo} />
+				<Input addTodo={this.addTodo} />
+			</div>
+		);
+	}
+}
+
+ReactDOM.render(
+	<App />,
+	document.getElementById('root')
+);
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin'); //installed via npm
+const webpack = require('webpack'); //to access built-in plugins
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: path.resolve(__dirname, 'app.js'),
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'public'),
+    watchContentBase: true,
+    port: 3000
+  },
+  resolve: {
+    modules: [path.resolve(__dirname, "src"), "node_modules"],
+    extensions: ['.js', '.jsx']
+  },
+  module: {
+    rules: [
+        {
+         test: /\.js$/,
+         exclude: /node_modules/,
+         loaders: 'babel-loader',
+         options:{
+           presets:["@babel/react", "@babel/preset-env"]
+         }
+       }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({template: './index.html'})
+  ]
+};
